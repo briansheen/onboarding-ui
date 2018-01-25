18deb3e8966d4538078f43519bda2227e234d6d7
Brian Sheen <brian.sheen@lt5904.local>
2018-01-25 10:16:44 -0600

    added css file and server updates for css

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,38 @@ const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 9000;
+var htmlFile;
+var cssFile;
 
-fs.readFile('index.html',(err,html) => {
+fs.readFile('index.html',function(err, data){
   if(err){
     throw err;
   }
+  htmlFile = data;
+});
 
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write(html);
-    res.end();
-  });
+fs.readFile('style.css', function(err, data){
+  if(err){
+    throw err;
+  }
+  cssFile = data;
+});
 
-  server.listen(port,hostname, () => {
-    console.log('Server running at http://'+hostname+':'+port+'/');
-  });
-})
+const server = http.createServer((req, res) => {
+  switch(req.url){
+    case "/style.css" :
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/css');
+      res.write(cssFile);
+      break;
+    default :
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(htmlFile);
+  }
+  res.end();
+});
+
+server.listen(port,hostname, () => {
+  console.log('Server running at http://'+hostname+':'+port+'/');
+});
