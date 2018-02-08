5eb7d70defab0bd4ac8a9ded1a151ba65890e662
Brian Sheen <brian.sheen@lt5904.local>
2018-02-08 16:25:10 -0600

    revised props for timeline.js

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 
 class Tweet extends React.Component {
   render() {
-    let props = this.props.tweet;
-    let user = props.twitterUser;
-    let date = new Date(props.createdAt);
+    let tweet = this.props.tweet;
+    let user = tweet.twitterUser;
+    let date = new Date(tweet.createdAt);
     let formattedDate = date.toLocaleDateString("en-US", {month:"short", day:"2-digit"});
     return React.createElement('div', {className: 'tweet'},
       React.createElement('div', {className: 'userInfo'},
@@ -16,8 +16,8 @@ class Tweet extends React.Component {
       ),
       React.createElement('div', {className: 'content'},
         React.createElement('div', {className: 'date'}, formattedDate),
-        React.createElement('a', {className: 'link', target: '_blank', href: `https://twitter.com/${user.twitterHandle}/status/${props.id}`},
-          React.createElement('div', {className: 'msg'}, `${props.message}`))
+        React.createElement('a', {className: 'link', target: '_blank', href: `https://twitter.com/${user.twitterHandle}/status/${tweet.id}`},
+          React.createElement('div', {className: 'msg'}, `${tweet.message}`))
       )
     );
   }
